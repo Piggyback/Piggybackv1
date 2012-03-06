b442d673b9c81c44b6ddb0fe9d7b54625cf344c2
Michael Gao <mgao.ucla@gmail.com>
2012-03-05 21:05:02 -0800

    Hard reset to pre-restkit branch. Added RestKit submodule. Pulled & merged changes from Kim's push. Manually updated project.pbxproj and now attempting to push with RestKit + Facebook SDK

--- Piggyback/PiggybackAppDelegate.h
+++ Piggyback/PiggybackAppDelegate.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "FBConnect.h"
 
-@interface PiggybackAppDelegate : UIResponder <UIApplicationDelegate, RKRequestDelegate>
+@interface PiggybackAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) Facebook *facebook;
