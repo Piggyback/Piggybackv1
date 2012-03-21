3d38748fcb4aeec6bb9d5f2b7b210df20c7c9923
Michael Gao <mgao.ucla@gmail.com>
2012-03-20 18:24:25 -0700

    Changed background color of navigation button to black

--- Piggyback/PiggybackNavigationController.m
+++ Piggyback/PiggybackNavigationController.m
@@ -12,7 +12,12 @@
 
 - (id)initWithRootViewController:(UIViewController *)rootViewController
 {
+    self.navigationBar.tintColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
     [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"piggyback_titlebar_background"] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                [UIColor blackColor], UITextAttributeTextColor,
+                                                [UIColor clearColor], UITextAttributeTextShadowColor,
+                                                nil]];
     return [super initWithRootViewController:rootViewController];
     
 }
@@ -20,7 +25,7 @@
 - (void) awakeFromNib
 {
     [super awakeFromNib];
-    self.navigationBar.tintColor = [UIColor yellowColor];
+    self.navigationBar.tintColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
     [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"piggyback_titlebar_background"] forBarMetrics:UIBarMetricsDefault];
     [self.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
                                                 [UIColor blackColor], UITextAttributeTextColor,
