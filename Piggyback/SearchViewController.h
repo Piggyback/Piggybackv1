8e9fe2b838397290bf1e1e2b60e0c3bb4cb75597
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2012-04-18 16:43:52 -0700

    search json results being returned and decoded

---
+++ Piggyback/SearchViewController.h
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.h
+//  Piggyback
+//
+//  Created by Kimberly Hsiao on 4/18/12.
+//  Copyright (c) 2012 Calimucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController <NSURLConnectionDelegate>
+
+@property (nonatomic, strong) NSMutableData *responseData;
+@property (nonatomic, strong) NSURLConnection* geocodeConnection;
+@property (nonatomic, strong) NSURLConnection* searchConnection;
+@property (weak, nonatomic) IBOutlet UITextField *query;
+@property (weak, nonatomic) IBOutlet UITextField *location;
+
+@end
---
+++ Piggyback/SearchViewController.m
@@ -0,0 +1,136 @@
+//
+//  SearchViewController.m
+//  Piggyback
+//
+//  Created by Kimberly Hsiao on 4/18/12.
+//  Copyright (c) 2012 Calimucho. All rights reserved.
+//
+
+#import "SearchViewController.h"
+#import "JSONKit.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+@synthesize responseData = _responseData;
+@synthesize geocodeConnection = _geocodeConnection;
+@synthesize searchConnection = _searchConnection;
+@synthesize query = _query;
+@synthesize location = _location;
+
+const NSString* radius = @"10000000";
+const NSString* intent = @"checkin";
+const NSString* limit = @"20";
+const NSString* clientID = @"LQYMHEIG05TK2HIQJGJ3MUGDNBAW1OKJKM4SSUFNYGSQMQIZ";
+const NSString* clientSecret = @"AXDTUGX5AA1DXDI2HUWVSODSFGKIK2RQYYGUWSUBDC0R5OLX";
+
+#pragma mark - getters and setters
+
+- (NSMutableData*)responseData {
+    if (_responseData == nil) {
+        _responseData = [[NSMutableData alloc] init];
+    }
+    return _responseData;
+}
+
+- (NSURLConnection*)geocodeConnection {
+    if (_geocodeConnection == nil) {
+        _geocodeConnection = [[NSURLConnection alloc] init];
+    }
+    return _geocodeConnection;
+}
+
+- (NSURLConnection*)searchConnection {
+    if (_searchConnection == nil) {
+        _searchConnection = [[NSURLConnection alloc] init];
+    }
+    return _searchConnection;
+}
+
+#pragma mark - nsurlconnection delegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+//    if (connection == self.geocodeConnection) {
+        [self.responseData setLength:0];
+//    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+//    if (connection == self.geocodeConnection) {
+        [self.responseData appendData:data];
+//    }
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    if (connection == self.geocodeConnection) {
+        NSLog(@"Geocode connection error");
+    } else if (connection == self.searchConnection) {
+        NSLog(@"Search connection error");
+    }
+    
+    NSLog(@"%@",[NSString stringWithFormat:@"Connection failed: %@", [error description]]);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    if (connection == self.geocodeConnection) {
+        // fetch lat and lng of requested location
+        NSDictionary *geocodeResponse = [[[NSString alloc] initWithData:self.responseData encoding:NSUTF8StringEncoding] objectFromJSONString];
+        NSString* lat = [[[[[geocodeResponse objectForKey:@"results"] objectAtIndex:0] objectForKey:@"geometry"] objectForKey:@"location"] objectForKey:@"lat"];
+        NSString* lng = [[[[[geocodeResponse objectForKey:@"results"] objectAtIndex:0] objectForKey:@"geometry"] objectForKey:@"location"] objectForKey:@"lng"];
+        NSString* latlng = [NSString stringWithFormat:@"%@%@%@",lat,@",",lng];
+        NSLog(@"%@",latlng);
+        
+        // call foursquare search API with retrieved lat lng        
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        [dateFormat setDateFormat:@"yyyyMMdd"];
+        NSDate* now = [NSDate date];
+        NSString *date = [dateFormat stringFromDate:now];
+        
+        NSURLRequest *searchRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@",@"https://api.foursquare.com/v2/venues/search?query=",self.query.text,@"&ll=",latlng,@"&radius=",radius,@"&intent=",intent,@"&limit=",limit,@"&client_id=",clientID,@"&client_secret=",clientSecret,@"&v=",date]]];
+        NSLog(@"%@",[NSString stringWithFormat:@"%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@",@"https://api.foursquare.com/v2/venues/search?query=",self.query.text,@"&ll=",latlng,@"&radius=",radius,@"&intent=",intent,@"&limit=",limit,@"&client_id=",clientID,@"&client_secret=",clientSecret,@"&v=",date]);
+        NSURLConnection *searchConnection = [[NSURLConnection alloc] initWithRequest:searchRequest delegate:self];
+        self.searchConnection = searchConnection;
+    }
+    
+    if (connection == self.searchConnection) {
+        NSDictionary *searchResponse = [[[NSString alloc] initWithData:self.responseData encoding:NSUTF8StringEncoding] objectFromJSONString];
+        NSLog(@"search response! %@",searchResponse);
+    }
+}
+
+#pragma mark - view lifecycle
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+        
+    // get lat and lng of specified location
+    NSURLRequest *geocodeRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@",@"https://maps.googleapis.com/maps/api/geocode/json?address=",self.location.text,@"&sensor=false"]]];
+    NSURLConnection *geocodeConnection = [[NSURLConnection alloc] initWithRequest:geocodeRequest delegate:self];
+    self.geocodeConnection = geocodeConnection;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
