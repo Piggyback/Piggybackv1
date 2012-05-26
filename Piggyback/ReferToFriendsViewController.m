fcf106a354fca517dd9077ee47fb70b2c69e89ee
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2012-05-25 18:00:35 -0700

    friends are alphabetized in refer to friend page

--- Piggyback/ReferToFriendsViewController.m
+++ Piggyback/ReferToFriendsViewController.m
@@ -161,10 +161,11 @@
     [super viewDidLoad];
     
     // set friends
-//    PBUser* currentUser = [(PiggybackAppDelegate *)[[UIApplication sharedApplication] delegate] currentUser];
     PBUser* currentUser = [PBUser findFirstByAttribute:@"userID" withValue:[[NSUserDefaults standardUserDefaults] objectForKey:@"UID"]];
-//    self.friends = [currentUser.friends allObjects];
-    self.friends = [currentUser.friends allObjects];
+    NSSortDescriptor *sortDescriptorFirstName = [[NSSortDescriptor alloc] initWithKey:@"firstName" ascending:YES];
+    NSSortDescriptor *sortDescriptorLastName = [[NSSortDescriptor alloc] initWithKey:@"lastName" ascending:YES];
+    NSArray *sortDescriptors = [NSArray arrayWithObjects:sortDescriptorFirstName,sortDescriptorLastName,nil];
+    self.friends = [[currentUser.friends allObjects] sortedArrayUsingDescriptors:sortDescriptors];
     
     NSLog(@"current user is %@",currentUser);
 //    NSLog(@"friends from app delegate are %@",currentUser.friends);
